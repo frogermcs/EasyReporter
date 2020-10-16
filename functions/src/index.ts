5854d706c4d8b634bcafa55ca1613c902a1c24a9
Miroslaw Stanek <froger.mcs@gmail.com>
2020-10-16 18:23:11 +0200

    added function to report progress points in batch

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,10 +3,4 @@ import * as admin from 'firebase-admin';
 admin.initializeApp()
 
 export { reportProgress } from './functions/report-progress'
-
-// export { reportReleasesBulk } from './functions/report-releases-bulk'
-// export { reportRelease } from './functions/report-release'
-// export { reportReleaseToBQ } from './functions/report-release-to-bq'
-// export { reportProgress } from './functions/report-progress'
-// export { reportProgressItemsBulk } from './functions/report-progress-points-bulk'
-// export { reportProgressToBQ } from './functions/report-progress-to-bq'
+export { reportProgressBulk } from './functions/report-progress-bulk'
