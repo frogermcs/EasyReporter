383775216dbdb4443bc14e850c60fd0c00c7d017
Miroslaw Stanek <froger.mcs@gmail.com>
2020-10-17 18:03:43 +0200

    added cloud function to rewrite new entries from Firestore to Big Query

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,3 +4,4 @@ admin.initializeApp()
 
 export { reportProgress } from './functions/report-progress'
 export { reportProgressBulk } from './functions/report-progress-bulk'
+export { reportProgressToBQ } from './functions/report-progress-to-bq'
